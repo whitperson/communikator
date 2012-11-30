4209ae144664979db5988f05d62a3a1284b0b817
whitperson <whitperson@gmail.com>
2012-11-30 15:52:38 -0500

    comm-server comm-client create

---
+++ comm-client/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommClient::Application.config.secret_token = 'b9de7d1d6eac6935981f0c017c19cb531c829e5892efbfd293901e4413645812a040718d91ea7c21677387c65df90f191aa00855ff3043fd8aa5822fdec23f4c'
---
+++ comm-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommServer::Application.config.secret_token = 'dfb7bf56d4a43478c4516eda8d28dc4247c9454d6931295326deb403f704ec2ca78cf6eee3265173f90ab9e26239b589d6c9bd2166eec2f1c6dbeb8bbb21289e'
