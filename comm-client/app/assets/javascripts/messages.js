e9f8d9acced6cdf843ebab1c6dad71916399320f
whitperson <whitperson@gmail.com>
2012-12-01 12:07:11 -0500

    tracking changes

---
+++ comm-client/app/assets/javascripts/messages.js
@@ -0,0 +1,26 @@
+$(function(){
+
+  $('#go_button').click(send_message);
+
+});
+
+function send_message()
+{
+  var msg_type = $('input:checked').val();
+  var to = $('#receiver').val();
+  var msg_text = $('#message_text').val();
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:3001/messages",
+    data: { msg_type: msg_type, to: to, msg_text: msg_text  }
+  }).done(function( msg )
+
+{
+  console.log("Data Saved: " + msg_text);
+  alert('text worked');
+
+});
+
+}
+
