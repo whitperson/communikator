479cbca456e1b14308c3379d96c4fa1213a08cf6
whitperson <whitperson@gmail.com>
2012-12-01 12:06:02 -0500

    all changes

--- comm-client/config/routes.rb
+++ comm-client/config/routes.rb
@@ -2,6 +2,7 @@ CommClient::Application.routes.draw do
 
   root :to => 'home#index'
 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- comm-server/config/initializers/ses_config.rb
+++
@@ -1,1 +0,0 @@
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base, :access_key_id => ENV['AWSKEY'], :secret_access_key => ENV['AWSSEC']
